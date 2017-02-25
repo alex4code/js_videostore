a58ff9015081fe6bcf8664d810e218471a53012c
Alexander   Velichko <alvi@mail.ua>
2017-02-25 12:34:32 +0200

    Рефакторинг, длбовление параметра и вывода в html

--- store.js
+++ store.js
@@ -1,18 +1,41 @@
 "use strict";
 
-function statement(customer, movies) {
-    let result = `Rental Record for ${customer.name}\n`;
-    for (let rental of customer.rentals) {
-        result += `\t${moveFor(rental).title}\t${getAmount(rental)}\n`;
+function statement(customer, movies, format) {
+    if (format === 'text') {
+        return statamentText();
+    } else if (format === 'html') {
+        return statamentHtml();
     }
-    // add footer lines
-    result += `Amount owed is ${getTotalAmount(customer)}\n`;
-    result += `You earned ${getTotalFrequentRenterPoints(customer)} frequent renter points\n`;
-    return result;
+
+    function statamentText() {
+        let result = `Rental Record for ${customer.name}\n`;
+        for (let rental of customer.rentals) {
+            result += `\t${moveFor(rental).title}\t${getAmount(rental)}\n`;
+        }
+        // add footer lines
+        result += `Amount owed is ${getTotalAmount(customer)}\n`;
+        result += `You earned ${getTotalFrequentRenterPoints(customer)} frequent renter points\n`;
+        return result;
+    }
+
+    function statamentHtml() {
+        let result = `<h1>Rental Record for <om>${customer.name}</om></h1>\n`;
+        result += '<table>\n';
+        for (let rental of customer.rentals) {
+            result += `<tr><td>${moveFor(rental).title}</td><td>${getAmount(rental)}</td></tr>\n`;
+        }
+        result += '</tabel>\n';
+        // add footer lines
+        result += `<p>Amount owed is <em>${getTotalAmount(customer)}</em></p>\n`;
+        result += `<p>You earned <em>${getTotalFrequentRenterPoints(customer)} </em> frequent renter points</p>\n`;
+        return result;
+    }
+
 
     function moveFor(rental) {
         return movies[rental.movieID]
     }
+
     function getAmount(rental) {
         let thisAmount = 0;
         let movie = moveFor(rental);
@@ -37,9 +60,11 @@ function statement(customer, movies) {
         }
         return thisAmount;
     }
+
     function getFrequentRenterPoints(rental) {
         return (moveFor(rental).code === "new" && rental.days > 2) ? 2 : 1;
     }
+
     function getTotalAmount(customer) {
         let totalAmount = 0;
         for (let rental of customer.rentals) {
@@ -47,6 +72,7 @@ function statement(customer, movies) {
         }
         return totalAmount;
     }
+
     function getTotalFrequentRenterPoints(customer) {
         let totalFrequentRenterPoints = 0;
         for (let rental of customer.rentals) {
@@ -80,4 +106,5 @@ let movies = {
     // etc
 };
 
-console.log(statement(customer, movies));
+console.log(statement(customer, movies, 'text'));
+console.log(statement(customer, movies, 'html'));
